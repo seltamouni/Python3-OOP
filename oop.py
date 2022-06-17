9296ce77dc826a1f0a41f261389583b217ff80bc
seltamouni <https://github.com/seltamouni>
2022-06-17 18:56:36 +0100

    introduction to OOP

---
+++ oop.py
@@ -0,0 +1,14 @@
+class Calculator():
+    def __init__(self):
+        print("hello world")
+
+    def sum(self,x,y):
+        print(x+y)
+    def mult(self,x,y):
+        print(x+y)
+    def sub(x,y):
+        print(x-y)
+        
+x = Calculator()
+print(x.sum(11,19))
+print(20)
